812380ee89970d53c5683ff0c6fe8611a09a61ef
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-31 09:56:29 +0200

    layoutIsDark

--- src/pages/PageUseToggle.jsx
+++ src/pages/PageUseToggle.jsx
@@ -2,6 +2,7 @@ import {useToggle} from '../hooks/useToggle';
 
 export const PageUseToggle = () => {
 	const [userIsOnline, toggleUserIsOnline] = useToggle(false);
+	const [layoutIsDark, toggleLayoutIsDark] = useToggle(true);
 
 	return (
 		<>
@@ -15,7 +16,20 @@ export const PageUseToggle = () => {
 				Toggle online status
 			</button>
 			<button onClick={() => toggleUserIsOnline(true)}>Login</button>
+
 			<button onClick={() => toggleUserIsOnline(false)}>Logout</button>
+
+			<hr/>
+			{layoutIsDark ? (
+				<p>Layout is dark.</p>
+			) : (
+				<p>Layout is light.</p>
+			)}
+			<button onClick={() => toggleLayoutIsDark()}>
+			Change layout	
+			</button>
+			<button onClick={() => toggleLayoutIsDark(true)}>set to dark</button>
+			<button onClick={() => toggleLayoutIsDark(false)}>set to light</button>
 		</>
 	);
 };
