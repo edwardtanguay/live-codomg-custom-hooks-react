9cade1c107400dfe4a272f30857308290fd5f188
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-31 09:23:55 +0200

    wrote code to use useToggle

--- src/pages/PageUseToggle.jsx
+++ src/pages/PageUseToggle.jsx
@@ -1,8 +1,17 @@
 export const PageUseToggle = () => {
+	const [userIsOnline, toggleUserIsOnline] = useToggle(false);
+
 	return (
 		<>
 			<h2>useToggle</h2>
-			<p>nnn</p>
+			{userIsOnline ? (
+				<p>User is currently online.</p>
+			) : (
+				<p>User is logged out.</p>
+			)}
+			<button onClick={() => toggleUserIsOnline()}>
+				Toggle online status
+			</button>
 		</>
 	);
 };
