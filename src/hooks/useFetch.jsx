b0bb2e3a353a4949b5c60244da5a5833b2e7eb1b
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-31 10:26:21 +0200

    finished

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export const useFetch = (url) => {
+export const useFetch = (url, useMockWait) => {
 	const [items, setItems] = useState([]);
 
 	useEffect(() => {
 		(async () => {
-			setItems((await axios.get(url)).data);
+			const _items = (await axios.get(url)).data;
+			if (useMockWait) {
+				setTimeout(() => {
+					setItems(_items);
+				}, Math.floor(Math.random() * 2000) + 1000);
+			} else {
+				setItems(_items);
+			}
 		})();
 	}, []);
 
-	return [items, items.length];
+	return [items, items.length, items.length === 0];
 };
--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -4,13 +4,31 @@ export const PageBooks = () => {
 	const [techBooks, totalBooks] = useFetch(
 		'https://edwardtanguay.netlify.app/share/techBooks.json'
 	);
-	const [employees] = useFetch('https://edwardtanguay.netlify.app/share/employees.json');
-	const [flashcards, totalFlashcards] = useFetch('https://edwardtanguay.netlify.app/share/flashcards.json');
+	const [employees, whatever, employeesAreLoading] = useFetch(
+		'https://edwardtanguay.netlify.app/share/employees.json', true
+	);
+	const [flashcards, totalFlashcards, flashcardsAreLoading] = useFetch(
+		'https://edwardtanguay.netlify.app/share/flashcards.json',
+		true
+	);
 
 	return (
 		<div className="page_books">
-			<p>Note: There are {employees.length} employees who have access to this page.</p>
-			<p>There are {totalFlashcards} flashcards about books.</p>
+			<p>
+				{employeesAreLoading ? (
+					<p>Employees are loading...</p>
+				) : (
+					<p>
+						Note: There are {employees.length} employees who have
+						access to this page.
+					</p>
+				)}
+			</p>
+			{flashcardsAreLoading ? (
+				<p>Flashcards are loading...</p>
+			) : (
+				<p>There are {totalFlashcards} flashcards about books.</p>
+			)}
 			<h2>There are {totalBooks} tech books.</h2>
 
 			<ul>
